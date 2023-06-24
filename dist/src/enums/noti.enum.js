f038a66f32c65abe57533a70e04140474965cf4d
Van Tri <vtri2016@gmail.com>
2023-06-24 14:16:32 +0700

    add noti type enum
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- dist/src/enums/noti.enum.js
+++ dist/src/enums/noti.enum.js
@@ -7,6 +7,8 @@ var NotiType;
     NotiType["COMMENT_POST"] = "COMMENT_POST";
     NotiType["COMMENT_REACTION"] = "COMMENT_REACTION";
     NotiType["REPLY_COMMENT"] = "REPLY_COMMENT";
+    NotiType["NEW_FRIEND_REQUEST"] = "NEW_FRIEND_REQUEST";
+    NotiType["FRIEND_REQUEST_ACCEPTED"] = "FRIEND_REQUEST_ACCEPTED";
 })(NotiType = exports.NotiType || (exports.NotiType = {}));
 var NotiSenderType;
 (function (NotiSenderType) {
